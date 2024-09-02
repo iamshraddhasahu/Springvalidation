0c0cffeb9eefc99771891c79c2b6ac3f47f29268
iamshraddhasahu <shraddhasahu2509@gmail.com>
2024-09-02 21:28:11 +0530

    customer validation msg

--- src/main/java/com/springmvc/Springvalidation/CustomerCotroller.java
+++ src/main/java/com/springmvc/Springvalidation/CustomerCotroller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerCotroller {
@@ -12,5 +13,12 @@ public String showForm(Model model){
     model.addAttribute("customer", new Customer());
     return"customer-form";
 }
+@PostMapping("/customerform")
+    public String customerform(Model model){
+
+    return"customer-confirmation";
+}
+
+
 
 }
