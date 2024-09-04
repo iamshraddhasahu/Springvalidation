fb76073559650a908e111da9cda531459666ff45
iamshraddhasahu <shraddhasahu2509@gmail.com>
2024-09-04 11:59:08 +0530

    spring boot

--- src/main/java/com/springmvc/Springvalidation/CustomerCotroller.java
+++ src/main/java/com/springmvc/Springvalidation/CustomerCotroller.java
@@ -2,15 +2,26 @@ package com.springmvc.Springvalidation;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerCotroller {
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+
+        StringTrimmerEditor ste = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class,ste);
+
+    }
+
 @GetMapping("/")
 public String showForm(Model model){
     model.addAttribute("customer", new Customer());
